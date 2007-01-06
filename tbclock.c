1ca17ecc8117684dcc5fcf635a3178506d13365b
tamentis <tamentis@neopulsar.org>
2007-01-06 16:18:07 +0000

    - You can disable frame and borders between blocks.  - Those options can be accessed via command line arguments.  - You can now use tbclock in transparent terminals.  - If terminal is too small, frame and borders are removed.

--- tbclock.c
+++ tbclock.c
@@ -1,6 +1,4 @@
 /*
- * $Id: tbclock.c,v 1.2 2007-01-06 14:10:42 tamentis Exp $
- *
  * Copyright (c) 2007 Bertrand Janin <tamentis@neopulsar.org>
  * All rights reserved.
  * 
@@ -26,12 +24,28 @@
  * SUCH DAMAGE.
  *
  * tbclock (Tamentis Binary Clock)
+ * ===============================
+ * This is a quick dirty little app to display a full screen binary
+ * clock in your terminal. If you don't know how to read this, just
+ * check out wikipedia for binary. It doesn't support resizing,
+ * there is a few lines in the source to do that, but disabled ;)
+ * It is known to work on Linux and OpenBSD, it's quite simple and
+ * should work everywhere...
  *
  * Compile with :
  * 	gcc -lncurses tbclock.c -Wall -O -o tbclock
  *
- * Changelog since 1.1.1.1
- * -----------------------
+ * --------------------------------------------------------------------
+ * Changelog since 1.2
+ * --------------------------------------------------------------------
+ *  	* You can disable frame and borders between blocks.
+ *  	* Those options can be accessed via command line arguments.
+ *  	* You can now use tbclock in transparent terminals.
+ *  	* If terminal is too small, frame and borders are removed 
+ *  	  automagically.
+ * --------------------------------------------------------------------
+ * Changelog since 1.1
+ * --------------------------------------------------------------------
  *	* Should work on any terminal resolution above 8x5
  *	* Should also work on monochrome terminals.
  *	* Blocks will scale depending of the size of the terminal.
@@ -47,6 +61,8 @@
 #include <time.h>
 #include <curses.h>
 #include <stdlib.h>
+
+#define TBCVER "$Id: tbclock.c,v 1.3 2007-01-06 16:18:07 tamentis Exp $"
 
 static WINDOW *screen;
 int now_sec, now_min, now_hour;
@@ -142,6 +158,29 @@ main(int ac, char **av)
 {
 	struct winsize ws;
 	WINDOW *mainwnd;
+	int frame = 1, ch, border = 1;
+
+	while ((ch = getopt(ac, av, "vfb")) != -1) {
+		switch (ch) {
+		case 'v':
+			fprintf(stderr, "%s\n", TBCVER);
+			exit(-1);
+		case 'f':
+			frame = 0;
+			break;
+		case 'b':
+			border = 0;
+			break;
+		default:
+			fprintf(stderr, "Usage: %s [-v] [-f]\n", av[0]);
+			fprintf(stderr, "  -f    Disable the frame\n");
+			fprintf(stderr, "  -b    Disable the borders between dots\n");
+			fprintf(stderr, "  -v    Show version\n");
+			exit(-1);
+		}
+	}
+	ac -= optind;
+	av += optind;
 
 	signal(SIGWINCH, resize);
 
@@ -150,12 +189,16 @@ main(int ac, char **av)
 		height = ws.ws_row;
 	}
 
-	/* Deny too small terminals */
-	if (height < 5 || width < 8) {
-		endwin();
-		fprintf(stderr, "The smallest allowed terminal is 8x5\n");
+	/* If terminal is really small, disable frame and borders */
+	if (height < 5 || width < 8)
+		frame = border = 0;
+
+	/* If terminal is too small, quit */
+	if (height < 3 || width < 6) {
+		fprintf(stderr, "The minimal allowed terminal size is 6x3 !\n");
 		exit(-1);
 	}
+
 
 	/* Screen Initialization */
 	mainwnd = initscr();
@@ -163,26 +206,44 @@ main(int ac, char **av)
 	cbreak();
 	nodelay(mainwnd, TRUE);
 
+
 	/* Prepare the 3 colors */
 	start_color();
-	init_pair(1, COLOR_BLUE, COLOR_BLUE);
-	init_pair(2, COLOR_RED, COLOR_RED);
-	init_pair(3, COLOR_YELLOW, COLOR_YELLOW);
+        if (use_default_colors() != ERR) {
+		init_pair(COLOR_BLACK, -1, -1);
+		init_pair(1, COLOR_BLUE, COLOR_BLUE);
+		init_pair(2, COLOR_RED, COLOR_RED);
+		init_pair(3, COLOR_YELLOW, COLOR_YELLOW);
+	}
 
-	/* Dot sizes */
-	dot_h  = height / 8; if (dot_h < 1) dot_h = 1;
-	dot_sh = height / 12;
-	dot_w  = width / 12; if (dot_w < 1) dot_w = 1;
-	dot_sw = width / 20;
-
-	/* Calculate margins... */
-	top_margin = (height - 3 * dot_h - dot_sh * 2) / 2;
-	left_margin = (width - 6 * dot_w - dot_sw * 5) / 2;
+	/* Calculate sizes and margins... */
+	if (frame) {
+		dot_h  = height / 8; if (dot_h < 1) dot_h = 1;
+		dot_w  = width / 12; if (dot_w < 1) dot_w = 1;
+		if (border) {
+			dot_sh = height / 12;
+			dot_sw = width / 20;
+		} else
+			dot_sh = dot_sw = 0;
+		top_margin = (height - 3 * dot_h - dot_sh * 2) / 2;
+		left_margin = (width - 6 * dot_w - dot_sw * 5) / 2;
+	} else {
+		dot_h  = height / 3; if (dot_h < 1) dot_h = 1;
+		dot_w  = width / 7; if (dot_w < 1) dot_w = 1;
+		if (border)
+			dot_sh = dot_sw = 1;
+		else
+			dot_sh = dot_sw = 0;
+		top_margin = (height - dot_h * 3 - dot_sh * 2) / 2;
+		left_margin = (width - dot_w * 6 - dot_sw * 5) / 2;
+	}
 
 	/* Prepare inside frame */
 	screen = newwin(height, width, 0, 0);
-	box(screen, ACS_VLINE, ACS_HLINE);
-	mvwprintw(screen, 0, width-10, "tbclock");
+	if (frame) {
+		box(screen, ACS_VLINE, ACS_HLINE);
+		mvwprintw(screen, 0, width-10, "tbclock");
+	}
 
 	/* Main loop */
 	for (;;) {
@@ -197,3 +258,4 @@ main(int ac, char **av)
 
 	return (0);
 }
+
